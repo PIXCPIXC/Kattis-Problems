7e72579003270ae3d55655b69ccf8c8ccb5cbffa
Pontus Ihlström <33262282+PIXCPIXC@users.noreply.github.com>
2017-11-10 11:24:03 +0100

    Add files via upload

---
+++ code/Bus Numbers/Bus Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> nrs = new List<int>();
+            string[] input = Console.ReadLine().Split(' ');
+
+
+
+            for (int i=0; i<n; i++)
+            {
+                nrs.Add(int.Parse(input[i]));
+            }
+
+            nrs.Sort();
+
+            int j = 0;
+
+            while(j < n)
+            {
+                int start = nrs[j];
+                while (j + 1 < n && nrs[j] == nrs[j + 1] - 1)
+                    j++;
+                int end = nrs[j];
+                string str = j == n - 1 ? "\n" : " ";
+                if (start == end)
+                    Console.Write(start+str);
+                else if (start == end - 1)
+                    Console.Write($"{start} {end}{str}");
+                else
+                    Console.Write($"{start}-{end}{str}");
+                j++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
